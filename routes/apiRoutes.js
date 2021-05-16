4f4a91e4508482c44c7ef7ecf48ac04963ea3bbb
MarketingPlus <benmarketingplus@gmail.com>
2021-05-16 17:42:20 +1000

    adde a post which should recieve a new note and add it to the db.json file then return the new note to the user

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,4 +10,27 @@ module.exports = function (app) {
             res.json(notes);
         });
     });
+
+    app.post('/api/notes', function (req, res) {
+        fs.readFile('db/db.json', 'utf8', (err, data) => {
+            if (err) throw err;
+
+            let notes = JSON.parse(data);
+
+            let newNote = req.body;
+            let uniqueId = (notes.length).toString();
+            newNote.id = uniqueId;
+            console.log(newNote);
+            notes.push(newNote);
+
+            fs.writeFileSync('db/db.json', JSON.stringify(notes), 'utf8', (err, data) => {
+                if (err) throw err;
+                console.log('Added a new note!');
+            });
+
+            res.json(notes);
+        });
+    });
+
+
 }
